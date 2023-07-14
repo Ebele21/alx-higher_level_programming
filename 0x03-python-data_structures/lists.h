f3c2ef57275d1b4053ebc1b127301cac7a6b772d
Ebele21 <ebele2121@gmail.com>
2023-07-14 04:25:04 -0700

    all

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Is a singly linked list
+ * @n: It is an integer
+ * @next:It points to the next node
+  * Description:It is a singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+void free_listint(listint_t *head);
+int is_palindrome(listint_t **head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+#endif /* LISTS_H */
