f1eb0a00ac60365e294f4d0aed63308a697d9035
Ebele21 <ebele2121@gmail.com>
2023-08-14 09:35:59 -0700

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This Contains the lookup function
+"""
+
+
+def lookup(obj):
+    """It returns a list of available attributes and methods of an object"""
+    return dir(obj)
